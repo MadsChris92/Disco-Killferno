cf42834e67b45a60b473ab40a199604997d2533a
MadsChris92 <madschris888@gmail.com>
2016-05-16 21:55:30 +0200

    dillerhans

---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Weapon : MonoBehaviour {
+
+
+    public GameObject bullet;
+    public float bulletSpeed = 100;
+    public Transform bulletSpawn;
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetMouseButtonDown(0))
+        {
+            GameObject bulletClone = Instantiate(bullet, bulletSpawn.transform.position, Quaternion.identity) as GameObject;
+            bulletClone.GetComponent<Rigidbody>().AddForce(transform.forward, ForceMode.Force);
+        }
+    }
+}
