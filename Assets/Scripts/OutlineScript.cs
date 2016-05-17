0bd95ae666dc67d0746d4b8869fed4e2a2c2bd9a
MadsChris92 <madschris888@gmail.com>
2016-05-17 18:43:29 +0200

    das

---
+++ Assets/Scripts/OutlineScript.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OutlineScript : MonoBehaviour
+{
+
+    // Use this for initialization
+    Color[] lightColors = new Color[6];
+
+    public GameObject[] lights = new GameObject[12];
+
+    float timer = 0;
+
+    public enum lightModes
+    {
+        staticLights, dynamicLights, reactingLights, partyLights
+    }
+    public lightModes myLightMode;
+
+    void Start()
+    {
+        lightColors[0] = Color.red;
+        lightColors[1] = Color.cyan;
+        lightColors[2] = Color.blue;
+        lightColors[3] = Color.green;
+        lightColors[4] = Color.yellow;
+        lightColors[5] = Color.magenta;
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(myLightMode == lightModes.partyLights)
+        {
+            partyLights();
+        }
+    }
+
+    void partyLights()
+    {
+        timer += Time.deltaTime;
+        if(timer > 5)
+        {
+            int activeLight = Random.Range(0, lightColors.Length);
+            for (int i = 0; i < lights.Length; i++)
+            {
+                lights[i].GetComponent<Renderer>().material.SetColor("_Emission", lightColors[activeLight]);   
+            }
+            timer = 0;
+        }
+    }
+}
