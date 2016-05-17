837be69b1b3b9c4053a90b90574c8e66b88e89a2
MadsChris92 <madschris888@gmail.com>
2016-05-17 09:08:44 +0200

    frsd

---
+++ Assets/Scripts/Objects.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Objects : MonoBehaviour {
+
+    // Use this for initialization
+    LineRenderer lr;
+    Vector3[] posisions = new Vector3[3];
+    
+	void Start () {
+        
+        for(int  i = 0; i < posisions.Length; i++)
+        {
+//            posisions[i] = new Vector3(gameObject.tra)
+        }
+
+        lr = gameObject.GetComponent<LineRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
